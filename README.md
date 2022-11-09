6e486a1d78fb5e26595714703a14ed5b9b95f619
Matheus Mendonca <mmendonca@microsoft.com>
2022-11-09 07:47:52 -0300

    Added palette to the commands menu

--- README.md
+++ README.md
@@ -4,4 +4,22 @@
 > conda create -n jupyterlab-ext --override-channels --strict-channel-priority -c conda-forge -c nodefaults jupyterlab=3 cookiecutter nodejs jupyter-packaging git
 > conda activate ext
 > cookiecutter https://github.com/jupyterlab/extension-cookiecutter-ts
+> cd [folder created by cookiecutter]
+> python3 -m pip install -ve .
+> jupyter labextension develop --overwrite .
+> jupyter labextension develop --overwrite .
+```
+
+Now we update the ``src/index.ts`` files, and then:
+
+```console
+> jlpm add @jupyterlab/apputils
+> jlpm add @jupyterlab/application
+> jlpm run build
+```
+
+Add the new imports. Then:
+
+```console
+> jlpm add @lumino/widgets
 ```
--- jupyter_apod/src/index.ts
+++ jupyter_apod/src/index.ts
@@ -3,15 +3,44 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+
+import { Widget } from '@lumino/widgets';
+
 /**
  * Initialization data for the jupyter-apod extension.
  */
-const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyter-apod:plugin',
-  autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyter-apod is activated!');
-  }
+const extension: JupyterFrontEndPlugin<void> = {
+    id: 'jupyter-apod',
+    autoStart: true,
+    requires: [ICommandPalette],
+    activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
+        console.log('JupyterLab extension jupyter_apod is activated!');
+
+        // Create a blank content widget inside of a MainAreaWidget
+        const content = new Widget();
+        const widget = new MainAreaWidget({ content });
+        widget.id = 'apod-jupyter';
+        widget.title.label = 'Astronomy Picture';
+        widget.title.closable = true;
+
+        // Add an application command
+        const command: string = 'apod:open';
+        app.commands.addCommand(command, {
+            label: 'Random Astronomy Picture',
+            execute: () => {
+                if (!widget.isAttached) {
+                    // Attach the widget to the main work area if it's not there
+                    app.shell.add(widget, 'main');
+                }
+                // Activate the widget
+                app.shell.activateById(widget.id);
+            }
+        });
+
+        // Add the command to the palette.
+        palette.addItem({ command, category: 'Tutorial' });
+    }
 };
 
-export default plugin;
+export default extension;
